086b408bebeef6d621112296e47ae79e084c27ab
dracu <dracu@thonkpad.thonkpad>
2025-06-29 15:35:45 +0530

    feat added swagger

---
+++ app/Swagger/SwaggerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="Hello API",
+ *     version="1.0.0",
+ *     description="Simple Hello World JSON API with Swagger in Laravel"
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Localhost API Server"
+ * )
+ */
+class SwaggerController
+{
+    // This is just a holder for annotations
+}
